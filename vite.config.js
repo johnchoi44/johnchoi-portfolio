840595fbf27d1969fa179fc17191ecd6e39cf47b
John Choi <John@Johns-MackBook-Pro.local>
2025-01-14 23:49:51 -0500

    update

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/johnchoi-portfolio",
+  base: "/portfolio",
   plugins: [react()],
   css: {
     modules: {
